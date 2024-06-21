cd057074d3c814f354f8063d3614b1765fafa98f
Gustavo Munis de Moraes <gustavomm29@hotmail.com>
2024-06-20 21:11:40 -0300

    adicionando logica nos it

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,35 @@
+import Comandos from '../support/Comandos.js'
+
+let teste
+let repete = 3
+
 describe('teste de apis', () => {
+  teste = false
+  repete = 30
+
+  for(let i = 0; i < repete; i++){
+    console.log(repete)
+    Comandos.its()
+  }
+
   it('retorna todos os livros', () => {
     cy.retornaLivros()
   })
 
-  it('nao encontra o livro especifico', () => {
-    cy.retornaLivro(1, 404)
-  })
+  if(teste == true){
+    it('nao encontra o livro especifico', () => {
+      cy.retornaLivro(1, 404)
+    })
+
+    it('nao encontra o livro especifico para edicao', () => {
+      cy.editaLivro(1, 404)
+    })
+
+    it('nao encontra o livro especifico para deletar', () => {
+      cy.deletaLivro(1, 404)
+      Comandos.teste123()
+    })
+  }
     
   it('cria livro', () => {
     cy.criaLivro(201)
@@ -15,19 +39,11 @@ describe('teste de apis', () => {
     cy.retornaLivro(2, 200)
   })
 
-  it('nao encontra o livro especifico para edicao', () => {
-    cy.editaLivro(1, 404)
-  })
-
   it('edita livro', () => {
     cy.editaLivro(2, 200)
   })
 
   it('deleta livro', () => {
     cy.deletaLivro(2, 200)
-  })
-
-  it('nao encontra o livro especifico para deletar', () => {
-    cy.deletaLivro(1, 404)
   })
 })
---
+++ cypress/support/Comandos.js
@@ -0,0 +1,14 @@
+class Comandos{
+
+    teste123(){
+        cy.log('testsssss')
+    }
+
+    its(){
+        it('testettettetete', () => {
+            cy.log('878787870')
+        })
+    }
+}
+
+export default new Comandos
