0d5cf3e87a1a3866c3432bf8566174b90693747f
Gustavo Munis de Moraes <gustavomm29@hotmail.com>
2024-06-26 15:39:07 -0300

    lascou

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,49 +1,68 @@
 import Comandos from '../support/Comandos.js'
 
-let teste
-let repete = 3
+let comandos = new Comandos()
 
 describe('teste de apis', () => {
-  teste = false
-  repete = 30
+  let repete = 10
+  let rand
+  // let livros = []
 
-  for(let i = 0; i < repete; i++){
-    console.log(repete)
-    Comandos.its()
-  }
-
-  it('retorna todos os livros', () => {
-    cy.retornaLivros()
+  it.only('prepara massa', () => {
+    const livros = comandos.preparaMassa()
+    // cy.log(livros)
+    // cy.log(livros.length)
+    // cy.log(livros[0])
+    // for (let i = 0; i < livros.length; i++) {
+    //   cy.log(`Item ID: ${livros[i].id}`)
+    // }
+    //  livros.each(it => cy.log(livros[it]))
+     
+    // console.log(await comandos.preparaMassa())
+    // livros.map(t => console.log(t))
   })
 
-  if(teste == true){
-    it('nao encontra o livro especifico', () => {
-      cy.retornaLivro(1, 404)
-    })
+  // for(let i = 0; i < repete; i++){  // roda varios
+  //   console.log(repete)
+  //   comandos.its()
+  // }
+
+  // it('retorna todos os livros', () => {
+  //   cy.retornaLivros()
+  // })
+
+  // if(comandos.teste == true){ // nao roda haha =)
+  //   let ultimoId = livros.length
+  //   ultimoId = livros[ultimoId]
+  //   idN404 = ultimoId +1
+  //   it('nao encontra o livro especifico', () => {
+  //     cy.retornaLivro(idN404, 404)
+  //   })
 
     it('nao encontra o livro especifico para edicao', () => {
-      cy.editaLivro(1, 404)
+      cy.editaLivro(10, 404)
     })
 
-    it('nao encontra o livro especifico para deletar', () => {
-      cy.deletaLivro(1, 404)
-      Comandos.teste123()
-    })
-  }
+  //   it('nao encontra o livro especifico para deletar', () => {
+  //     cy.deletaLivro(idN404, 404)
+  //     Comandos.teste123()
+  //   })
+  // }
     
-  it('cria livro', () => {
-    cy.criaLivro(201)
-  })
+  // it('cria livro', () => {
+  //   cy.criaLivro(201)
+  // })
 
-  it('encontra o livro especifico', () => {
-    cy.retornaLivro(2, 200)
-  })
+  // it('encontra o livro especifico', () => {
+  //   rand = comandos.livros.length
+  //   cy.log(rand)
+  //   // cy.retornaLivro(Comandos.livros[rand].id, 200)
+  // })
 
-  it('edita livro', () => {
-    cy.editaLivro(2, 200)
-  })
+  // it('edita livro', () => {
+  //   cy.editaLivro(rand, 200)
+  // })
 
-  it('deleta livro', () => {
-    cy.deletaLivro(2, 200)
-  })
+  // it('deleta livro', () => {
+  //   cy.deletaLivro(rand, 200)
+  // })
 })
--- cypress/support/Comandos.js
+++ cypress/support/Comandos.js
@@ -1,5 +1,29 @@
 class Comandos{
 
+    constructor(){
+        this.livros = []
+        // teste = false
+    }
+
+    async preparaMassa(){ 
+        let teste = []
+        cy.request('/livros').then(resp => {
+            expect(resp.status).to.eq(200)
+            if(JSON.parse(resp.body).length == 0){
+                cy.log('entrou')
+                cy.criaLivro(201)
+            }else{
+                teste.push(JSON.parse(resp.body)[0])
+                cy.log(teste.length)
+                cy.log(teste[0].id)
+            }
+        })
+        cy.log(teste.length)
+        cy.log(teste)
+        // cy.log(typeof teste)
+        // return teste
+    }
+
     teste123(){
         cy.log('testsssss')
     }
@@ -11,4 +35,4 @@ class Comandos{
     }
 }
 
-export default new Comandos
+export default Comandos
